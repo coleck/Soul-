22a291858d5a3a54872f5fd1aa5718e6b569e7e8
coleck <col3ck@gmail.com>
2025-11-20 22:14:27 +0300

    Add files via upload

--- js/total.js
+++ js/total.js
@@ -1,3 +1,43 @@
+// Объект с ссылками для каждого архетипа
+const archetypeLinks = {
+  analyst: {
+    movie: "https://www.kinopoisk.ru/film/397667/",
+    route: "https://yandex.ru/maps/-/CLG3RV39",
+    playlist:
+      "https://music.yandex.ru/playlists/89b3674a-8540-9cf3-25cb-8882a8f84158?utm_source=desktop&utm_medium=copy_link",
+  },
+  dreamer: {
+    movie: "https://www.kinopoisk.ru/film/841081/",
+    route: "https://yandex.ru/maps/-/CLG3fVJe",
+    playlist:
+      "https://music.yandex.ru/playlists/54bfc6de-1963-848d-d2e9-f5f8a847a915?utm_source=desktop&utm_medium=copy_link",
+  },
+  aesthetic: {
+    movie: "https://www.kinopoisk.ru/film/17721/",
+    route: "https://yandex.ru/maps/-/CLG36DZz",
+    playlist:
+      "https://music.yandex.ru/playlists/326e2039-4c6e-b20b-8d61-a3c6ba4609b3?utm_source=desktop&utm_medium=copy_link",
+  },
+  detective: {
+    movie: "https://www.kinopoisk.ru/film/958442/",
+    route: "https://yandex.ru/maps/-/CLG3ZW1a",
+    playlist:
+      "https://music.yandex.ru/playlists/8c73db93-7005-7fb5-787c-f2f393d2f8b3?utm_source=desktop&utm_medium=copy_link",
+  },
+  intuitive: {
+    movie: "https://www.kinopoisk.ru/film/43911/",
+    route: "https://yandex.ru/maps/-/CLG3FZMF",
+    playlist:
+      "https://music.yandex.ru/playlists/c4bd61a3-b753-1cd5-2ba3-2475e70886fe?utm_source=desktop&utm_medium=copy_link",
+  },
+  seeker: {
+    movie: "https://www.kinopoisk.ru/film/454522/",
+    route: "https://yandex.ru/maps/-/CLG3J09Q",
+    playlist:
+      "https://music.yandex.ru/playlists/fa257a8f-78d7-5f54-6fea-935ffc3c5dfa?utm_source=desktop&utm_medium=copy_link",
+  },
+};
+
 // Функция для получения параметра из URL
 function getUrlParameter(name) {
   name = name.replace(/[\[\]]/g, "\\$&");
@@ -8,6 +48,37 @@ function getUrlParameter(name) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+// Функция для обновления ссылок в зависимости от архетипа
+function updateRecommendationLinks(archetype) {
+  const links = archetypeLinks[archetype] || archetypeLinks.analyst;
+
+  const movieBlock = document.querySelector(".score_movie");
+  const routeBlock = document.querySelector(".score_route");
+  const playlistBlock = document.querySelector(".score_playlist");
+
+  if (movieBlock) movieBlock.setAttribute("data-link", links.movie);
+  if (routeBlock) routeBlock.setAttribute("data-link", links.route);
+  if (playlistBlock) playlistBlock.setAttribute("data-link", links.playlist);
+}
+
+// Обработчики для блоков рекомендаций (универсальные)
+function setupRecommendationHandlers() {
+  const movieBlock = document.querySelector(".score_movie");
+  const routeBlock = document.querySelector(".score_route");
+  const playlistBlock = document.querySelector(".score_playlist");
+
+  const handleClick = function () {
+    const link = this.getAttribute("data-link");
+    if (link) {
+      window.open(link, "_blank");
+    }
+  };
+
+  if (movieBlock) movieBlock.addEventListener("click", handleClick);
+  if (routeBlock) routeBlock.addEventListener("click", handleClick);
+  if (playlistBlock) playlistBlock.addEventListener("click", handleClick);
+}
+
 // Текущий отображаемый архетип
 let currentArchetype = "";
 
@@ -15,6 +86,12 @@ let currentArchetype = "";
 document.addEventListener("DOMContentLoaded", function () {
   const archetype = getUrlParameter("archetype") || "analyst";
   currentArchetype = archetype;
+
+  // Обновляем ссылки для текущего архетипа
+  updateRecommendationLinks(archetype);
+
+  // Настраиваем обработчики
+  setupRecommendationHandlers();
 
   // Показываем контейнер с нужным архетипом
   const targetArchetypeContainer = document.getElementById(archetype);
@@ -66,37 +143,6 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
-
-  // Обработчики для блоков рекомендаций
-  const movieBlock = document.querySelector(".score_movie");
-  const routeBlock = document.querySelector(".score_route");
-  const playlistBlock = document.querySelector(".score_playlist");
-
-  if (movieBlock) {
-    movieBlock.addEventListener("click", function () {
-      const link = this.getAttribute("data-link");
-      window.open(link, "_blank");
-    });
-  }
-
-  if (routeBlock) {
-    routeBlock.addEventListener("click", function () {
-      const link = this.getAttribute("data-link");
-      window.open(link, "_blank");
-    });
-  }
-
-  if (playlistBlock) {
-    playlistBlock.addEventListener("click", function () {
-      const link = this.getAttribute("data-link");
-      window.open(link, "_blank");
-    });
-  }
-});
-
-// Добавь в конец файла обработку тач-событий
-document.addEventListener("DOMContentLoaded", function () {
-  // ... существующий код ...
 
   // Оптимизация для мобильных устройств
   if ("ontouchstart" in window) {
